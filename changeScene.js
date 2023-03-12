66ff0375e5e218bea289d8515e3dd5d4c0e8a245
kintal2026 <38499600+DatBlueDev@users.noreply.github.com>
2023-03-12 16:56:58 +0800

    Add files via upload

--- changeScene.js
+++ changeScene.js
@@ -2,12 +2,23 @@ var TitleScreen = document.getElementById("TitleScreen");
 var HomeScreen = document.getElementById("HomeScreen");
 
 var GameScene = document.getElementById("GameScene");
+var GameOverScene = document.getElementById("HomeScreen");
+
 var LevelScreen = document.getElementById("Levels");
 
 function showLevels(){
     HomeScreen.style.display = "none";
 
     LevelScreen.style.display = "block";
+
+}
+function showGameOver(){
+    console.log("game over");
+    GameScene.remove();
+    GameScene.style.display = "none";
+
+
+    //GameOverScene.style.display = "block";
 
 }
 function showAbout(){}
